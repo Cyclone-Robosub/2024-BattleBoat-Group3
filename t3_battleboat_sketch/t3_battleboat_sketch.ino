4859077649ac96cb49724eeb01810756be987b4e
Ziqiang (Joe) Zhu <ziqzhu@ucdavis.edu>
2024-10-27 12:15:50 -0700

    done for first try with just joy stick

--- t3_battleboat_sketch/t3_battleboat_sketch.ino
+++ t3_battleboat_sketch/t3_battleboat_sketch.ino
@@ -15,19 +15,22 @@
 //#define OVERRIDE_REMOTE_CONTROL 0.5
 #define DO_PRINTOUT
 
+// global variables
+float l_req;
+float r_req;
+
+// p1, p2 motor pin for direction, ps motor pin for speed, req requested speed
 void drive_single(uint32_t p1, uint32_t p2, uint32_t ps, float req) {
   uint32_t spd = (uint32_t)(abs(req) * MAXSPEED);
-  if (abs(req) <= 0.1) {
+  if (abs(req) <= 0.1) { // motor stop
     digitalWrite(p1, LOW);
     digitalWrite(p2, LOW);
-  } else if (req < 0) {
-    digitalWrite(p1, LOW);
-    digitalWrite(p2, HIGH);
-  } else {
+  } else if (req > 0.1) { // motor forward
     digitalWrite(p1, HIGH);
     digitalWrite(p2, LOW);
   }
 
+  // set speed
   analogWrite(ps, spd);
 }
 
@@ -47,35 +50,50 @@ void setup() {
 
   Serial.begin(250000);
   Dabble.begin(9600);
+
+  // set motor speed to 0
+  l_req = 0.0;
+  r_req = 0.0;
+}
+
+void process_degree(int degree) {
+  if (degree == 90) {
+    l_req = 1.0;
+    r_req = 1.0;
+  } else if (315 <= degree && degree <= 0) {
+    // turn right
+    l_req = 1.0;
+    r_req = 0;
+  } else if (180 <= degree && degree < 225) {
+    // turn left
+    l_req = 0;
+    r_req = 1.0;
+  } else if (0 < degree && degree <= 90) {
+    // percentage right turn
+    r_req = degree / 90.0;
+    l_req = 1.0;
+  } else if (90 < degree && degree <= 180) {
+    // percentage left turn
+    r_req = 1.0;
+    l_req = (180 - degree) / 90.0;
+  }
+}
+
+void process_radious(int radious) {
+  l_req *= radious / 7.0;
+  r_req *= radious / 7.0;
 }
 
 void loop() {
   Dabble.processInput();
 
-  float l_req = 0.0;
-  float r_req = 0.0;
+  int a = GamePad.getAngle();
 
-#ifdef OVERRIDE_REMOTE_CONTROL
-  l_req = 0.5;
-  r_req = 0.5;
-#else
-  float steer = GamePad.getXaxisData() / 7.0;
+  process_degree(a);
 
-  float fwd = (GamePad.isCirclePressed() ? 1.0 : (GamePad.isCrossPressed() ? -1.0 : 0.0));
-  float afwd = GamePad.getYaxisData() / 7.0;
+  int r = GamePad.getRadius();
 
-  if (abs(afwd) > 0.2) {
-    fwd += afwd;
-  }
-
-  l_req = fwd - steer;
-  r_req = fwd + steer;
-  
-  if (l_req > 1.0) l_req = 1.0;
-  if (l_req < -1.0) l_req = -1.0;
-  if (r_req > 1.0) r_req = 1.0;
-  if (r_req < -1.0) r_req = -1.0;
-#endif
+  process_radius(a);
 
   drive(l_req, r_req);
 
